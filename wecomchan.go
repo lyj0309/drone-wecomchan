44b818e2148d6e458d2416a0771335ffc89f7880
lyj <2457395722@qq.com>
2021-12-17 15:40:51 +0800

    first

--- wecomchan.go
+++ wecomchan.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -10,23 +9,16 @@ import (
 
 func main() {
 	key := os.Getenv("PLUGIN_KEY")
-	title := os.Getenv("PLUGIN_TEXT")
-	desp := os.Getenv("PLUGIN_DESP")
-	openid := os.Getenv("PLUGIN_OPENID")
+	path := os.Getenv("PLUGIN_PATH")
+	msg := os.Getenv("PLUGIN_MSG")
 
-	if key == "" || title == "" {
-		log.Fatalln("key or title[text] is required")
+	if key == "" || msg == "" || path == "" {
+		log.Fatalln("key or title[text] or path is required")
 	}
 
-	if len(title) > 32 {
-		log.Fatalln("title[text] is longer than 32 characters")
-	}
-
-	reqURL := fmt.Sprintf("https://sctapi.ftqq.com/%s.send", key)
-	res, err := http.PostForm(reqURL, url.Values{
-		"title":  []string{title},
-		"desp":   []string{desp},
-		"openid": []string{openid},
+	res, err := http.PostForm(path, url.Values{
+		"sendkey": []string{key},
+		"msg":     []string{msg},
 	})
 	if err != nil {
 		log.Fatalln("post error:", err)
